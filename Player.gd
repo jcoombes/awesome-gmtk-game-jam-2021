a10cefe2a9ee3ac57ffe0e85ba3eeede74bfa480
Jamie C <coombes.jd@gmail.com>
2021-06-12 20:05:22 +0100

    TWO SNAKES

--- Player.gd
+++ Player.gd
@@ -1,10 +1,7 @@
-extends KinematicBody2D
+extends Node2D
 
-var avg = Vector2()
-var diff = Vector2()
-var speed = 200
-var cursor_positions = []
-var velocity = Vector2(0, 0)
+signal death
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -12,19 +9,18 @@ var velocity = Vector2(0, 0)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	pass # Replace with function body.
+	pass
 
 func _input(event):
-	if event is InputEventMouseMotion:
-		self.velocity = event.relative.normalized() * 100
-#		look_at(event.relative * 10)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	move_and_slide(self.velocity)
-	
-	var buddy = add_child()
+	pass
+
+
+func _on_Head_death():
+	emit_signal("death")
